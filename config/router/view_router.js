36399039c74d0f98733b4a8dbb21b77013cb1167
shot013 <shot013@naver.com>
2020-01-13 14:02:36 +0900

    20200113
    메인, 지나온길 update

--- config/router/view_router.js
+++ config/router/view_router.js
@@ -4,6 +4,11 @@ module.exports = (app) => {
         console.log('index로');
         res.render('./index.html');
     });
+    
+    app.get('/myways', (req, res) => {
+        console.log('myways로');
+        res.render('./myways.html');
+    });
 
     app.get('/guestbook', (req, res) => {
         console.log('guestbook으로');
